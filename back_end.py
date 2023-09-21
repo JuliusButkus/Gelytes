ece2448f3bc619212ca2f7623d880865b5ad4cc2
Julius Butkus <j.butkus.skouds@gmail.com>
2023-09-21 08:26:40 +0300

    suskirstytos kodu zonos

--- back_end.py
+++ back_end.py
@@ -1,1 +1,26 @@
 
+
+
+
+
+
+# Gėliu pridėjimas  Julius
+
+
+# spalvu pridėjimas Ilija
+
+
+# mėnsiu suvedimas turėtu but suvesta
+
+
+# zonu pridėjimas    Dainius
+
+
+# Gėliu priskyrimas zonoms    Ruslanas
+
+
+# Filtravimas paga zonas pagal spalvas pagal geles    Mindaugui
+
+
+
+# papildimoa funkcija kiek laiko tesiasi zydėjimas
